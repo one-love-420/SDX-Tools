3416c9c857f2a4053013cbaacf5df42ccb7ed899
joeykrim <joeykrim@gmail.com>
2011-12-31 21:33:01 -0500

    Added root detection Added dialog for no root access Cleaned up tabs and whitespace

--- sdxtools/src/com/dodgejcr/sdxtools/SdxtoolsActivity.java
+++ sdxtools/src/com/dodgejcr/sdxtools/SdxtoolsActivity.java
@@ -1,112 +1,166 @@
 package com.dodgejcr.sdxtools;
 
+import java.io.DataOutputStream;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-
 
 public class SdxtoolsActivity extends Activity {
-	
+
+    private boolean rootAccess = false;
+    static final int DIALOG_NO_ROOT_ACCESS = 0;
+
+    private static final String LOG_TAG = "SDXTools";
+
     /** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
-	 
-	    MyPagerAdapter adapter = new MyPagerAdapter();
-	    ViewPager myPager = (ViewPager) findViewById(R.id.toolspager);
-	    myPager.setAdapter(adapter);
-	    myPager.setCurrentItem(0);
-	}
-private class MyPagerAdapter extends PagerAdapter {
-    	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        MyPagerAdapter adapter = new MyPagerAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.toolspager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+        rootAccess = canSU();
+        if (!rootAccess) showDialog(DIALOG_NO_ROOT_ACCESS);
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+    
         public int getCount() {
             return 9;
         }
- 
+    
         public Object instantiateItem(View collection, int position) {
- 
+    
             LayoutInflater inflater = (LayoutInflater) collection.getContext()
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
- 
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    
             int resId = 0;
             switch (position) {
-            case 0:
-                resId = R.layout.menu;
-                break;
-            case 1:
-                resId = R.layout.phoneinfo;
-                break;
-            case 2:
-                resId = R.layout.appremoval;
-                break;
-            case 3:
-            	resId = R.layout.filemanager;
-            	break;
-            case 4:
-            	resId = R.layout.scriptmanager;
-            	break;
-            case 5:
-                resId = R.layout.logcat;
-                break;
-            case 6:
-            	resId = R.layout.phonedump;
-            	break;
-            case 7:
-            	resId = R.layout.root;
-            	break;
-            case 8: 
-            	resId = R.layout.kernel;
-            	break;          
+                case 0:
+                    resId = R.layout.menu;
+                    break;
+                case 1:
+                    resId = R.layout.phoneinfo;
+                    break;
+                case 2:
+                    resId = R.layout.appremoval;
+                    break;
+                case 3:
+                    resId = R.layout.filemanager;
+                    break;
+                case 4:
+                    resId = R.layout.scriptmanager;
+                    break;
+                case 5:
+                    resId = R.layout.logcat;
+                    break;
+                case 6:
+                    resId = R.layout.phonedump;
+                    break;
+                //support removed by dodgejcr
+                //case 7:
+                    //resId = R.layout.root;
+                    //break;
+                case 7:
+                    resId = R.layout.kernel;
+                    break;
             }
- 
+    
             View view = inflater.inflate(resId, null);
- 
             ((ViewPager) collection).addView(view, 0);
- 
             return view;
         }
- 
+    
         @Override
         public void destroyItem(View arg0, int arg1, Object arg2) {
             ((ViewPager) arg0).removeView((View) arg2);
- 
         }
- 
+    
         @Override
         public boolean isViewFromObject(View arg0, Object arg1) {
             return arg0 == ((View) arg1);
- 
         }
- 
+    
         @Override
         public Parcelable saveState() {
             return null;
         }
+    
+        @Override
+        public void finishUpdate(View arg0) {
+            // TODO Auto-generated method stub
+        }
+    
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+            // TODO Auto-generated method stub
+        }
+    
+        @Override
+        public void startUpdate(View arg0) {
+            // TODO Auto-generated method stub
+        }
+    }
 
-		@Override
-		public void finishUpdate(View arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+    /** http://www.monkeycancode.com/android-show-a-legal-agreement-dialog-before-program-launches */
+    @Override
+    public Dialog onCreateDialog(int id){
+        Dialog dialog;
+        switch(id) {
+        case DIALOG_NO_ROOT_ACCESS:
+            Dialog dialog1 = new AlertDialog.Builder(this)
+            .setMessage(R.string.noRootMessage)
+            .setCancelable(false)
+            .setTitle(R.string.dialogTitleNoRoot)
+            .setPositiveButton(R.string.allAboutRoot, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    String url = getString(R.string.allAboutRootMarketLink);
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(i);
+                    finish();
+                }
+            })
+            .setNegativeButton(R.string.limitedMode, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //contine on our merry way
+                }
+            })
+            .create();
+            return dialog1;
+        default:
+            dialog = null;
+        }
+        return dialog;
+    }
 
-		@Override
-		public void restoreState(Parcelable arg0, ClassLoader arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void startUpdate(View arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-}
+    //source - https://github.com/joeykrim/WimaxKeys/blob/master/src/com/joeykrim/wimaxkeys/WimaxKeys.java#L259
+    private boolean canSU() {
+        Process process = null;
+        int exitValue = -1;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            DataOutputStream toProcess = new DataOutputStream(process.getOutputStream());
+            toProcess.writeBytes("exec id\n");
+            toProcess.flush();
+            exitValue = process.waitFor();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error determining root access: " + e);
+            exitValue = -1;
+        }
+        return exitValue == 0;
+    }
 }
